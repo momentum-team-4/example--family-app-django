682703182d454dd82337a5444bc2c2f7e7266cd3
Clinton Dreisbach <clinton@dreisbach.us>
2020-10-26 16:29:26 -0400

    Add pagination

--- project/settings.py
+++ project/settings.py
@@ -157,7 +157,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    # 'PAGE_SIZE': 5
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
